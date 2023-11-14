56ca086491c4495f48dd5127bcfe851e110bbb36
in-deepaknegi <negideepak.in@gmail.com>
2023-11-14 16:04:58 +0530

    Project setup

---
+++ components/home/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar1 from '@/components/navbar/Navbar1'
+import Navbar2 from '@/components/navbar/Navbar2'
+
+
+const Main = () => {
+    return (
+        <>
+            <Navbar1 />
+            <Navbar2 />
+        </>
+    )
+}
+
+export default Main
---
+++ components/navbar/Navbar1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar1 = () => {
+    return (
+        <nav>
+            Navbar 1
+        </nav>
+
+    )
+}
+
+export default Navbar1
