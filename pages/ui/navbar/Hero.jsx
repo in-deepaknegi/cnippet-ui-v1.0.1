1ccbe858ee4cf83c00ee8391240ec2e75428a7ea
in-deepaknegi <negideepak.in@gmail.com>
2023-11-18 21:23:06 +0530

    minor mixes in navbar page

--- pages/ui/navbar/Hero.jsx
+++ pages/ui/navbar/Hero.jsx
@@ -4,11 +4,15 @@ import React, { useState } from 'react'
 const Hero = () => {
     const [activeTab1, setActiveTab1] = useState(0);
     const [activeTab2, setActiveTab2] = useState(0);
+    const [activeTab3, setActiveTab3] = useState(0);
 
     const changeTab1 = (index) => {
         setActiveTab1(index);
     };
     const changeTab2 = (index) => {
+        setActiveTab2(index);
+    };
+    const changeTab3 = (index) => {
         setActiveTab2(index);
     };
 
@@ -185,14 +189,14 @@ const Hero = () => {
                             <div className='ml-6 flex items-center'>
                                 <div className='flex space-x-1 rounded-lg bg-slate-100 p-0.5'>
                                     <button
-                                        onClick={() => changeTab2(0)}
-                                        className={`flex items-center rounded-md py-[0.45rem] pl-2 pr-2 text-sm font-semibold lg:pr-3 ${activeTab2 === 0 ? 'bg-white shadow' : ''}`}>
+                                        onClick={() => changeTab3(0)}
+                                        className={`flex items-center rounded-md py-[0.45rem] pl-2 pr-2 text-sm font-semibold lg:pr-3 ${activeTab3 === 0 ? 'bg-white shadow' : ''}`}>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#0950f6" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z" /><circle cx="12" cy="12" r="3" /></svg>
                                         <span className='sr-only lg:not-sr-only lg:ml-2 text-stone-900'>Preview</span>
                                     </button>
                                     <button
-                                        onClick={() => changeTab2(1)}
-                                        className={`flex items-center rounded-md py-[0.45rem] pl-2 pr-2 text-sm font-semibold lg:pr-3 ${activeTab2 === 1 ? 'bg-white shadow' : ''}`}>
+                                        onClick={() => changeTab3(1)}
+                                        className={`flex items-center rounded-md py-[0.45rem] pl-2 pr-2 text-sm font-semibold lg:pr-3 ${activeTab3 === 1 ? 'bg-white shadow' : ''}`}>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#3e75f4" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="16 18 22 12 16 6" /><polyline points="8 6 2 12 8 18" /></svg>
                                         <span className='sr-only lg:not-sr-only lg:ml-2 text-stone-900'>Code</span>
                                     </button>
@@ -207,7 +211,7 @@ const Hero = () => {
                             </div>
 
                             <div className='col-span-2 row-start-2 min-w-0 mt-3 border rounded-2xl'>
-                                {activeTab2 === 0 ? (
+                                {activeTab3 === 0 ? (
                                     <>
                                         <header className="">
                                             <nav className="flex items-center justify-between p-7 lg:px-8">
