60434d1adf6eed93ad0bbc75a5ec18a845fc5fae
in-deepaknegi <negideepak.in@gmail.com>
2023-11-19 21:35:39 +0530

    updated hero components page

--- pages/ui/hero/Hero.jsx
+++ pages/ui/hero/Hero.jsx
@@ -1,5 +1,7 @@
 "use client"
 import React, { useState } from 'react'
+import Hero1 from '@/public/hero1.jpg';
+import Image from 'next/image';
 
 const Hero = () => {
     const [activeTab1, setActiveTab1] = useState(0);
@@ -68,11 +70,40 @@ const Hero = () => {
                                 </button>
                             </div>
 
-                            <div className='col-span-2 row-start-2 min-w-0 mt-3 border rounded-2xl'>
+                            <div className='col-span-2 row-start-2 min-w-0 mt-3 border rounded-2xl overflow-hidden'>
                                 {activeTab1 === 0 ? (
                                     <>
-                                        <section>
-                                            Hero
+                                        <section className='relative bg-white '>
+                                            <div className='grid max-w-7xl grid-cols-12 mx-auto lg:gap-8'>
+                                                <div className='px-6 pb-24 pt-10 sm:pb-32 col-span-7 lg:px-0 lg:pb-56 lg:pt-48 xl:col-span-6'>
+                                                    <div className='mx-auto max-w-2xl lg:mx-6'>
+                                                        <div className="hidden sm:mb-8 sm:flex">
+                                                            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                                                                Announcing our next round of funding. <a href="#" className="font-semibold text-indigo-600"><span className="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
+                                                            </div>
+                                                        </div>
+                                                        <h1 className='text-6xl font-semibold'>
+                                                            Data to enrich your online bussiness
+                                                        </h1>
+                                                        <p className='mt-6 text-lg'>
+                                                            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+                                                        </p>
+                                                        <div className="mt-10 flex gap-x-6">
+                                                            <a href="#" className="rounded-md bg-purple-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Browse Components</a>
+                                                            <a href="#" className="text-sm font-semibold leading-6 text-gray-900 my-auto">
+                                                                Explore templates <span aria-hidden="true">→</span></a>
+                                                        </div>
+
+                                                    </div>
+                                                </div>
+                                                <div className='relative lg:col-span-6 lg:mr-8 xl:inset-0 xl:left-0 xl:mr-0'>
+                                                    <Image
+                                                        src={Hero1}
+                                                        alt='hero image 1'
+                                                        className='aspect-[3/2] w-full object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full'
+                                                    />
+                                                </div>
+                                            </div>
                                         </section>
                                         {/* <div className='bg-gray-100 h-[10rem]'></div> */}
                                     </>
