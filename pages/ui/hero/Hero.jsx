e2a1d9fec472e7e924cd3c4bafff4bda324675f6
in-deepaknegi <negideepak.in@gmail.com>
2023-11-20 11:43:24 +0530

    added new component -2 in hero header

--- pages/ui/hero/Hero.jsx
+++ pages/ui/hero/Hero.jsx
@@ -230,7 +230,6 @@ const Hero = () => {
                                                 </div>
                                             </div>
                                         </section>
-                                        {/* <div className='bg-gray-100 h-[10rem]'></div> */}
                                     </>
                                 ) : (
                                     <div>code</div>
@@ -244,7 +243,7 @@ const Hero = () => {
                             <div className='flex min-w-0'>
                                 <h2 className='truncate text-base font-medium leading-7 text-slate-900'>
                                     <a href="">
-                                        Simple navbar with menu of left and button on right
+                                        Simple centered with backgroud image
                                     </a>
                                 </h2>
                                 <p className='ml-3 hidden whitespace-nowrap rounded-lg bg-slate-100 px-2 py-0.5 text-xs font-semibold leading-6 text-slate-700 lg:block'>
@@ -278,10 +277,34 @@ const Hero = () => {
                             <div className='col-span-2 row-start-2 min-w-0 mt-3 border rounded-2xl'>
                                 {activeTab3 === 0 ? (
                                     <>
-                                        <section>
-                                            Hero
+                                        <section className='relative isolate overflow-hidden'>
+                                            <Image
+                                                src={Hero3}
+                                                alt='hero image 3'
+                                                className='absolute inset-0 z-[-10] h-full w-full object-cover rounded-2xl blur-[2px]'
+                                            />
+                                            <div className='absolute w-full h-full z-[-9] bg-gradient-to-b from-black/10 via-black/10 to-black rounded-2xl'></div>
+                                            <div className='mx-auto max-w-2xl py-32 sm:py-48 lg:py-56'>
+                                                <div className="hidden sm:mb-8 sm:flex">
+                                                    <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                                                        Announcing our next round of funding. <a href="#" className="font-semibold text-indigo-600"><span className="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
+                                                    </div>
+                                                </div>
+                                                <h1 className='text-left text-6xl font-semibold'>
+                                                    Data to enrich your online bussiness
+                                                </h1>
+                                                <p className='mt-6 text-left text-lg text-gray-200 font-light'>
+                                                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+                                                </p>
+                                                <div className="mt-10 flex gap-x-6">
+                                                    <a href="#" className="rounded-md bg-purple-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                                        Get Started
+                                                    </a>
+                                                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900 my-auto">
+                                                        Read more <span aria-hidden="true">→</span></a>
+                                                </div>
+                                            </div>
                                         </section>
-                                        {/* <div className='bg-gray-100 h-[10rem]'></div> */}
                                     </>
                                 ) : (
                                     <div>code</div>
