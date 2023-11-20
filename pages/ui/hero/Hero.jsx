cae9dde59ac59292832b984cd119b6e57fc44af6
in-deepaknegi <negideepak.in@gmail.com>
2023-11-20 10:47:08 +0530

    added new component in hero headers

--- pages/ui/hero/Hero.jsx
+++ pages/ui/hero/Hero.jsx
@@ -2,6 +2,11 @@
 import React, { useState } from 'react'
 import Hero1 from '@/public/hero1.jpg';
 import Hero2 from '@/public/hero2.jpg';
+import Hero3 from '@/public/hero3.jpg';
+import Hero4 from '@/public/hero4.jpg';
+import Hero5 from '@/public/hero5.jpg';
+import Hero6 from '@/public/hero6.jpg';
+
 
 import Image from 'next/image';
 
@@ -95,7 +100,6 @@ const Hero = () => {
                                                             <a href="#" className="text-sm font-semibold leading-6 text-gray-900 my-auto">
                                                                 Explore templates <span aria-hidden="true">→</span></a>
                                                         </div>
-
                                                     </div>
                                                 </div>
                                                 <div className='relative lg:col-span-6 lg:mr-8 xl:inset-0 xl:left-0 xl:mr-0'>
@@ -155,8 +159,76 @@ const Hero = () => {
                             <div className='col-span-2 row-start-2 min-w-0 mt-3 border rounded-2xl'>
                                 {activeTab2 === 0 ? (
                                     <>
-                                        <section>
-                                            Hero
+                                        <section className='relative isolate'>
+                                            <div className='overflow-hidden'>
+                                                <div className='mx-auto max-w-7xl px-6 pb-16 pt-16 lg:px-8'>
+                                                    <div className='mx-auto max-w-2xl gap-14 lg:mx-0 lg:flex lg:max-w-none lg:text-center'>
+                                                        <div className='w-full max-w-xl lg:flex-shrink-0 lg:max-w-2xl my-auto'>
+                                                            <div className="hidden sm:mb-8 sm:flex">
+                                                                <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                                                                    Announcing our next round of funding. <a href="#" className="font-semibold text-indigo-600"><span className="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
+                                                                </div>
+                                                            </div>
+                                                            <h1 className='text-left text-6xl font-semibold'>
+                                                                Data to enrich your online bussiness
+                                                            </h1>
+                                                            <p className='mt-6 text-left text-lg text-gray-600 font-light'>
+                                                                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+                                                            </p>
+                                                            <div className="mt-10 flex gap-x-6">
+                                                                <a href="#" className="rounded-md bg-purple-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                                                    Get Started
+                                                                </a>
+                                                                <a href="#" className="text-sm font-semibold leading-6 text-gray-900 my-auto">
+                                                                    Read more <span aria-hidden="true">→</span></a>
+                                                            </div>
+                                                        </div>
+                                                        <div className='mt-14 flex justify-end gap-8 sm:mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0'>
+                                                            <div className='ml-auto w-44 flex-none pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-[0] xl:pt-80'>
+                                                                <div className='relative'>
+                                                                    <Image
+                                                                        src={Hero2}
+                                                                        alt='hero image 2'
+                                                                        className='w-full h-full aspect-[2/3] rounded-2xl bg-gray-50 object-cover shadow-xl'
+                                                                    />
+                                                                </div>
+                                                            </div>
+                                                            <div className='ml-auto w-44 flex-none pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-[0] xl:pt-30'>
+                                                                <div className='relative'>
+                                                                    <Image
+                                                                        src={Hero3}
+                                                                        alt='hero image 2'
+                                                                        className='w-full h-full aspect-[2/3] rounded-2xl bg-gray-50 object-cover shadow-xl'
+                                                                    />
+                                                                </div>
+                                                                <div className='relative'>
+                                                                    <Image
+                                                                        src={Hero4}
+                                                                        alt='hero image 2'
+                                                                        className='mt-8 w-full h-full aspect-[2/3] rounded-2xl bg-gray-50 object-cover shadow-xl'
+                                                                    />
+                                                                </div>
+                                                            </div>
+                                                            <div className='w-44 flex-none sm:ml-0 lg:order-last xl:order-[0] xl:mt-[-2rem]'>
+                                                                <div className='relative'>
+                                                                    <Image
+                                                                        src={Hero5}
+                                                                        alt='hero image 2'
+                                                                        className='w-full h-full aspect-[2/3] rounded-2xl bg-gray-50 object-cover shadow-xl'
+                                                                    />
+                                                                </div>
+                                                                <div className='relative'>
+                                                                    <Image
+                                                                        src={Hero6}
+                                                                        alt='hero image 2'
+                                                                        className='mt-8 w-full h-full aspect-[2/3] rounded-2xl bg-gray-50 object-cover shadow-xl'
+                                                                    />
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </section>
                                         {/* <div className='bg-gray-100 h-[10rem]'></div> */}
                                     </>
