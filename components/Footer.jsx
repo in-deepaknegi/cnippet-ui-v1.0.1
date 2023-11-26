48a351c6da6102606bbb59e0db4bf8919ac8b75a
in-deepaknegi <negideepak.in@gmail.com>
2023-11-26 17:32:11 +0530

    added folder-stut->newsletter & updated components list section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
                 <div className='sm:flex sm:items-center sm:justify-between'>
                     <a href="https://flowbite.com/" className='flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse'>
                         {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" /> */}
-                        <span className='self-center text-2xl font-semibold whitespace-nowrap'>CnippetUI</span>
+                        <span className='self-center text-2xl font-medium whitespace-nowrap'>CnippetUI</span>
                     </a>
                     <ul className='flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 '>
                         <li>
--- pages/home/Components.jsx
+++ pages/home/Components.jsx
@@ -2,6 +2,57 @@ import Image from 'next/image'
 import React from 'react'
 import Nav from '@/public/navbars.png'
 
+const component = [
+    {
+        name: 'Navbar',
+        source: '/components/application-ui/navbar',
+        imageURL: Nav,
+        number: '3 components',
+    },
+    {
+        name: 'Footer',
+        source: '/components/application-ui/footer',
+        imageURL: Nav,
+        number: '2 components',
+    },
+    {
+        name: 'Hero Header',
+        source: '/components/application-ui/hero',
+        imageURL: Nav,
+        number: '4 components',
+    },
+    {
+        name: 'Testimonials',
+        source: '/components/application-ui/testimonial',
+        imageURL: Nav,
+        number: '4 components',
+    },
+    {
+        name: 'Contact',
+        source: '/components/application-ui/contact',
+        imageURL: Nav,
+        number: '4 components',
+    },
+    {
+        name: 'Team',
+        source: '/components/application-ui/team',
+        imageURL: Nav,
+        number: '4 components',
+    },
+    {
+        name: 'FAQ',
+        source: '/components/application-ui/faq',
+        imageURL: Nav,
+        number: '4 components',
+    },
+    {
+        name: 'Newsletter',
+        source: '/components/application-ui/newsletter',
+        imageURL: Nav,
+        number: '4 components',
+    },
+]
+
 const Components = () => {
     return (
         <section className='mx-auto w-full px-4 sm:px-6 lg:px-8'>
@@ -19,132 +70,27 @@ const Components = () => {
 
             <div className='relative mt-10 md:mt-8'>
                 <ul className='grid grid-cols-[repeat(auto-fill,minmax(12rem, 1fr))] gap-x-8 gap-y-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-                    <li>
-                        <div className='group relative before:absolute before:-inset-2.5 before:rounded-[20px] before:bg-gray-50 before:opacity-0 hover:before:opacity-100'>
-                            <div className='relative aspect-[2/1] overflow-hidden rounded-lg bg-gray-100 ring-1 ring-gray-900/10'>
-                                <Image
-                                    src={Nav}
-                                    alt='navbar-img'
-                                    className='absolute inset-0 h-full w-full'
-                                />
+                    {component.map((component) => (
+                        <li key={component.name}>
+                            <div className='group relative before:absolute before:-inset-2.5 before:rounded-[20px] before:bg-gray-50 before:opacity-0 hover:before:opacity-100'>
+                                <div className='relative aspect-[2/1] overflow-hidden rounded-lg bg-gray-100 ring-1 ring-gray-900/10'>
+                                    <Image
+                                        src={component.imageURL}
+                                        alt='navbar-img'
+                                        className='absolute inset-0 h-full w-full'
+                                    />
+                                </div>
+                                <h4 className='mt-4 text-sm font-medium text-slate-900 group-hover:text-indigo-600'>
+                                    <a href={component.source}>
+                                        <span className='absolute -inset-2.5 z-10'></span>
+                                        <span className='relative'>{component.name}</span>
+                                    </a>
+                                </h4>
+                                <p className='relative mt-1.5 text-xs font-medium text-slate-500'>{component.number}</p>
                             </div>
-                            <h4 className='mt-4 text-sm font-medium text-slate-900 group-hover:text-indigo-600'>
-                                <a href="/components/application-ui/navbar">
-                                    <span className='absolute -inset-2.5 z-10'></span>
-                                    <span className='relative'>Navbar</span>
-                                </a>
-                            </h4>
-                            <p className='relative mt-1.5 text-xs font-medium text-slate-500'>4 components</p>
-                        </div>
-                    </li>
-                    <li>
-                        <div className='relative before:absolute before:-inset-2.5 before:rounded-[20px] before:bg-gray-50 before:opacity-0 hover:before:opacity-100'>
-                            <div className='relative aspect-[2/1] overflow-hidden rounded-lg bg-gray-100 ring-1 ring-gray-900/10'>
-                                <Image
-                                    src={Nav}
-                                    alt='navbar-img'
-                                    className='absolute inset-0 h-full w-full'
-                                />
-                            </div>
-                            <h4 className='mt-4 text-sm font-medium text-slate-900 group-hover:text-indigo-600'>
-                                <a href="/components/application-ui/footer">
-                                    <span className='absolute -inset-2.5 z-10'></span>
-                                    <span className='relative'>Footer</span>
-                                </a>
-                            </h4>
-                            <p className='relative mt-1.5 text-xs font-medium text-slate-500'>4 components</p>
-                        </div>
-                    </li>
-                    <li>
-                        <div className='relative before:absolute before:-inset-2.5 before:rounded-[20px] before:bg-gray-50 before:opacity-0 hover:before:opacity-100'>
-                            <div className='relative aspect-[2/1] overflow-hidden rounded-lg bg-gray-100 ring-1 ring-gray-900/10'>
-                                <Image
-                                    src={Nav}
-                                    alt='navbar-img'
-                                    className='absolute inset-0 h-full w-full'
-                                />
-                            </div>
-                            <h4 className='mt-4 text-sm font-medium text-slate-900 group-hover:text-indigo-600'>
-                                <a href="/components/application-ui/hero">
-                                    <span className='absolute -inset-2.5 z-10'></span>
-                                    <span className='relative'>Hero Header</span>
-                                </a>
-                            </h4>
-                            <p className='relative mt-1.5 text-xs font-medium text-slate-500'>4 components</p>
-                        </div>
-                    </li>
-                    <li>
-                        <div className='relative before:absolute before:-inset-2.5 before:rounded-[20px] before:bg-gray-50 before:opacity-0 hover:before:opacity-100'>
-                            <div className='relative aspect-[2/1] overflow-hidden rounded-lg bg-gray-100 ring-1 ring-gray-900/10'>
-                                <Image
-                                    src={Nav}
-                                    alt='navbar-img'
-                                    className='absolute inset-0 h-full w-full'
-                                />
-                            </div>
-                            <h4 className='mt-4 text-sm font-medium text-slate-900 group-hover:text-indigo-600'>
-                                <a href="/components/application-ui/testimonial">
-                                    <span className='absolute -inset-2.5 z-10'></span>
-                                    <span className='relative'>Testimonials</span>
-                                </a>
-                            </h4>
-                            <p className='relative mt-1.5 text-xs font-medium text-slate-500'>4 components</p>
-                        </div>
-                    </li>
-                    <li>
-                        <div className='relative before:absolute before:-inset-2.5 before:rounded-[20px] before:bg-gray-50 before:opacity-0 hover:before:opacity-100'>
-                            <div className='relative aspect-[2/1] overflow-hidden rounded-lg bg-gray-100 ring-1 ring-gray-900/10'>
-                                <Image
-                                    src={Nav}
-                                    alt='navbar-img'
-                                    className='absolute inset-0 h-full w-full'
-                                />
-                            </div>
-                            <h4 className='mt-4 text-sm font-medium text-slate-900 group-hover:text-indigo-600'>
-                                <a href="/components/application-ui/contact">
-                                    <span className='absolute -inset-2.5 z-10'></span>
-                                    <span className='relative'>Contact</span>
-                                </a>
-                            </h4>
-                            <p className='relative mt-1.5 text-xs font-medium text-slate-500'>4 components</p>
-                        </div>
-                    </li>
-                    <li>
-                        <div className='relative before:absolute before:-inset-2.5 before:rounded-[20px] before:bg-gray-50 before:opacity-0 hover:before:opacity-100'>
-                            <div className='relative aspect-[2/1] overflow-hidden rounded-lg bg-gray-100 ring-1 ring-gray-900/10'>
-                                <Image
-                                    src={Nav}
-                                    alt='navbar-img'
-                                    className='absolute inset-0 h-full w-full'
-                                />
-                            </div>
-                            <h4 className='mt-4 text-sm font-medium text-slate-900 group-hover:text-indigo-600'>
-                                <a href="/components/application-ui/team">
-                                    <span className='absolute -inset-2.5 z-10'></span>
-                                    <span className='relative'>Team</span>
-                                </a>
-                            </h4>
-                            <p className='relative mt-1.5 text-xs font-medium text-slate-500'>4 components</p>
-                        </div>
-                    </li>
-                    <li>
-                        <div className='relative before:absolute before:-inset-2.5 before:rounded-[20px] before:bg-gray-50 before:opacity-0 hover:before:opacity-100'>
-                            <div className='relative aspect-[2/1] overflow-hidden rounded-lg bg-gray-100 ring-1 ring-gray-900/10'>
-                                <Image
-                                    src={Nav}
-                                    alt='navbar-img'
-                                    className='absolute inset-0 h-full w-full'
-                                />
-                            </div>
-                            <h4 className='mt-4 text-sm font-medium text-slate-900 group-hover:text-indigo-600'>
-                                <a href="/components/application-ui/faq">
-                                    <span className='absolute -inset-2.5 z-10'></span>
-                                    <span className='relative'>FAQ</span>
-                                </a>
-                            </h4>
-                            <p className='relative mt-1.5 text-xs font-medium text-slate-500'>4 components</p>
-                        </div>
-                    </li>
+                        </li>
+                    ))}
+                    
                     <div className="pointer-events-none absolute -inset-x-2.5 -bottom-2.5 h-[16.6%] bg-gradient-to-t from-white to-white/25 sm:h-[33.3%]"></div>
                 </ul>
             </div>
