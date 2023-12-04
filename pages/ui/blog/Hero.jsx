d35665578a8acd7b04cf5b0b90e48dfafb9b5d56
in-deepaknegi <negideepak.in@gmail.com>
2023-12-04 11:40:31 +0530

    updated blogs section #3

--- pages/ui/blog/Hero.jsx
+++ pages/ui/blog/Hero.jsx
@@ -333,9 +333,52 @@ const Hero = () => {
                             <div className='col-span-2 row-start-2 min-w-0 mt-3'>
                                 {activeTab3 === 0 ? (
                                     <>
-                                        <footer className='border rounded-2xl'>
-                                            footer
-                                        </footer>
+                                        <section className="bg-white py-24 border rounded-2xl sm:py-32">
+                                            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                                                <div className="mx-auto max-w-2xl text-center">
+                                                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
+                                                    <p className="mt-2 text-lg leading-8 text-gray-600">
+                                                        Learn how to grow your business with our expert advice.
+                                                    </p>
+                                                </div>
+                                                <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                                                    {posts.map((post) => (
+                                                        <article key={post.id} className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
+                                                            <Image
+                                                                src={post.img}
+                                                                alt='post-img'
+                                                                className='absolute inset-0 -z-10 w-full h-full object-cover'
+                                                            />
+                                                            <div className='absolute inset-0 z-[-10] bg-gradient-to-t from-[#111827] 
+                                                            to-[#ffffff00]
+                                                            '></div>
+
+                                                            <h3 className="text-lg font-semibold leading-6 text-gray-300 ">
+                                                                <a href={post.href}>
+                                                                    <span className="absolute inset-0" />
+                                                                    {post.title}
+                                                                </a>
+                                                            </h3>
+                                                            <div className='mt-3 flex flex-wrap items-center gap-x-4 gap-y-1 overflow-hidden leading-6 text-sm text-gray-300'>
+                                                                <time dateTime={post.datetime} className="">
+                                                                    {post.date}
+                                                                </time>
+                                                                <div className='flex items-center gap-x-4'>
+                                                                    <div className='flex gap-x-3'>
+                                                                        <Image src={post.author.imageUrl} alt='id-1' className="h-10 w-10 rounded-full object-cover bg-gray-50" />
+                                                                        <div className='my-auto'>
+                                                                            {post.author.name}
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+
+
+                                                        </article>
+                                                    ))}
+                                                </div>
+                                            </div>
+                                        </section>
                                     </>
                                 ) : (
                                     <div>code</div>
