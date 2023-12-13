57d7d0e029fde2550f980d99b042940777d8c9fc
in-deepaknegi <negideepak.in@gmail.com>
2023-12-13 21:53:10 +0530

    updated navbar section

--- pages/ui/navbar/Hero.jsx
+++ pages/ui/navbar/Hero.jsx
@@ -5,6 +5,7 @@ const Hero = () => {
     const [activeTab1, setActiveTab1] = useState(0);
     const [activeTab2, setActiveTab2] = useState(0);
     const [activeTab3, setActiveTab3] = useState(0);
+    const [activeTab4, setActiveTab4] = useState(0);
 
     const changeTab1 = (index) => {
         setActiveTab1(index);
@@ -14,6 +15,9 @@ const Hero = () => {
     };
     const changeTab3 = (index) => {
         setActiveTab3(index);
+    };
+    const changeTab4 = (index) => {
+        setActiveTab4(index);
     };
 
     return (
@@ -107,7 +111,7 @@ const Hero = () => {
                             <div className='flex min-w-0'>
                                 <h2 className='truncate text-base font-medium leading-7 text-slate-900'>
                                     <a href="">
-                                    Simple navbar with menu in center and button in right
+                                        Simple navbar with menu in center and button in right
                                     </a>
                                 </h2>
                                 <p className='ml-3 hidden whitespace-nowrap rounded-lg bg-slate-100 px-2 py-0.5 text-xs font-semibold leading-6 text-slate-700 lg:block'>
@@ -232,6 +236,97 @@ const Hero = () => {
                                                         </span>
                                                     </a>
                                                 </div>
+                                            </nav>
+                                        </header>
+                                    </>
+                                ) : (
+                                    <div>code</div>
+                                )}
+                            </div>
+                        </div>
+                    </section>
+
+                    <section id='navbar-3'>
+                        <div className='grid grid-cols-[1fr,auto] items-center'>
+                            <div className='flex min-w-0'>
+                                <h2 className='truncate text-base font-medium leading-7 text-slate-900'>
+                                    <a href="">
+                                        Simple navbar with menu of left and button on right
+                                    </a>
+                                </h2>
+                                <p className='ml-3 hidden whitespace-nowrap rounded-lg bg-slate-100 px-2 py-0.5 text-xs font-semibold leading-6 text-slate-700 lg:block'>
+                                    Requires JS
+                                </p>
+                            </div>
+                            <div className='ml-6 flex items-center'>
+                                <div className='flex space-x-1 rounded-lg bg-slate-100 p-0.5'>
+                                    <button
+                                        onClick={() => changeTab4(0)}
+                                        className={`flex items-center rounded-md py-[0.45rem] pl-2 pr-2 text-sm font-semibold lg:pr-3 ${activeTab4 === 0 ? 'bg-white shadow' : ''}`}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#0950f6" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z" /><circle cx="12" cy="12" r="3" /></svg>
+                                        <span className='sr-only lg:not-sr-only lg:ml-2 text-stone-900'>Preview</span>
+                                    </button>
+                                    <button
+                                        onClick={() => changeTab4(1)}
+                                        className={`flex items-center rounded-md py-[0.45rem] pl-2 pr-2 text-sm font-semibold lg:pr-3 ${activeTab4 === 1 ? 'bg-white shadow' : ''}`}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#3e75f4" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="16 18 22 12 16 6" /><polyline points="8 6 2 12 8 18" /></svg>
+                                        <span className='sr-only lg:not-sr-only lg:ml-2 text-stone-900'>Code</span>
+                                    </button>
+                                </div>
+                                <div className='ml-6 mr-3 hidden h-5 w-px bg-slate-900 sm:block'></div>
+                                <div className='relative hidden sm:block text-slate-900 font-medium sm:text-sm pl-3.5 pr-[1.875rem]'>
+                                    NextJs
+                                </div>
+                                <button>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#c9c9c9" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="14" height="14" x="8" y="8" rx="2" ry="2" /><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" /></svg>
+                                </button>
+                            </div>
+
+                            <div className='col-span-2 row-start-2 min-w-0 mt-3'>
+                                {activeTab4 === 0 ? (
+                                    <>
+                                        <header className='relative z-10 border rounded-2xl overflow-hidden'>
+                                            <nav>
+                                                <div className='bg-black'>
+                                                    <div className='mx-auto flex h-10 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8'>
+                                                        <div className='hidden text-white text-sm lg:block lg:flex-1'>
+                                                            English
+                                                        </div>
+                                                        <div className='flex-1 items-center lg:flex-none'>
+                                                            <a href="/" className='text-gray-200 text-sm'>
+                                                                Get free delivery on orders over $100
+                                                            </a>
+                                                        </div>
+                                                        <div className='hidden lg:flex lg:flex-1 lg:items-center lg:justify-end'>
+                                                            <a href="/" className='text-gray-100 text-sm'>Create an account</a>
+                                                            <span className='h-4 w-[1px] bg-gray-200 mx-2'></span>
+                                                            <a href="/" className='text-gray-100 text-sm'>Sign In</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className='bg-white border-b border-gray-300 flex items-center justify-between lg:py-5 lg:px-8'>
+                                                    <div className="flex lg:flex-none">
+                                                        <a href="/" className="-m-1.5 p-1.5 text-2xl font-medium">
+                                                            Cnippet
+                                                            <span className="">UI</span>
+                                                        </a>
+                                                    </div>
+                                                    <div className="hidden lg:flex lg:flex-1 lg:gap-x-12 ml-10">
+                                                        <a href="/components" className="text-sm font-semibold leading-6 text-gray-900">Components</a>
+                                                        <a href="/features" className="text-sm font-semibold leading-6 text-gray-900">Templates</a>
+                                                        <a href="/contact" className="text-sm font-semibold leading-6 text-gray-900">Contacts</a>
+                                                        <a href="/about" className="text-sm font-semibold leading-6 text-gray-900">About</a>
+                                                    </div>
+                                                    <div className="hidden lg:ml-8 lg:flex lg:flex-none lg:gap-4 lg:items-center lg:pl-8">
+                                                        <a href="/login" className='text-gray-700'>Search</a>
+                                                        <a href="/help" className='text-gray-700'>Help</a>
+                                                        <span className='h-5 w-[1px] bg-gray-500'></span>
+                                                        <a href="">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" className='w-5 h-5 text-gray-600'><path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"></path></svg>
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                                <div className='h-8 bg-stripes-white bg-gray-200'></div>
                                             </nav>
                                         </header>
                                     </>
