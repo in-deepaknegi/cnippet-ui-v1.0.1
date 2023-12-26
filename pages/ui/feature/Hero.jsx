4f2a1217b84d05b11b1bfb86e2c9d687dc61beb1
in-deepaknegi <negideepak.in@gmail.com>
2023-12-26 21:47:21 +0530

    updated feature component#1

--- pages/ui/feature/Hero.jsx
+++ pages/ui/feature/Hero.jsx
@@ -1,5 +1,23 @@
 "use client"
 import React, { useState } from 'react'
+import Image from 'next/image';
+import Hero1 from '@/public/hero4.jpg';
+
+const features = [
+    {
+        name: 'Push to deploy.',
+        description:
+            'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
+    },
+    {
+        name: 'SSL certificates.',
+        description: "Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.",
+    },
+    {
+        name: 'Database backups.',
+        description: 'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
+    },
+]
 
 const Hero = () => {
     const [activeTab1, setActiveTab1] = useState(0);
@@ -70,8 +88,42 @@ const Hero = () => {
 
                             <div className='col-span-2 row-start-2 min-w-0 mt-3'>
                                 {activeTab1 === 0 ? (
-                                    <section className='bg-white border rounded-2x'>
-                                        feature
+                                    <section className='overflow-hidden bg-white border rounded-2xl py-16 sm:py-20'>
+                                        <div className='mx-auto px-6 lg:px-8'>
+                                            <div className='mx-auto grid gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none grid-cols-12'>
+                                                <div className='col-span-12 lg:pr-8 lg:pt-4 lg:col-span-7'>
+                                                    <div className=''>
+                                                        <h2 className='mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl'>
+                                                            Everything you need to deploy your app
+                                                        </h2>
+                                                        <p className='mt-6 text-base text-gray-700 md:text-lg'>
+                                                            Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum pulvinar et feugiat blandit at. In mi viverra elit nunc.
+                                                        </p>
+                                                        <ul className='mt-10 ml-5 max-w-xl text-base leading-7 text-gray-600 lg:max-w-none'>
+                                                            {features.map((feature) => (
+                                                                <li key={feature.name} className='list-disc'>
+                                                                    {feature.description}
+                                                                </li>
+                                                            ))}
+                                                        </ul>
+                                                        <div className="mt-10 flex gap-x-6">
+                                                            <a href="#" className="rounded-md px-3.5 py-2.5 text-sm font-semibold text-blue-600 shadow-sm ring-1 ring-blue-500 hover:text-white hover:bg-blue-500">Discover more</a>
+                                                            <a href="#" className="text-sm font-semibold leading-6 text-gray-900 my-auto">
+                                                                Join us <span aria-hidden="true">→</span></a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className='col-span-12 lg:col-span-5'>
+                                                    <Image
+                                                        src={Hero1}
+                                                        alt='Product screenshot'
+                                                        quality={100}
+                                                        loading='eager'
+                                                        className='h-[20rem] lg:h-[35rem] object-cover rounded-2xl'
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
                                     </section>
                                 ) : (
                                     <div>code</div>
