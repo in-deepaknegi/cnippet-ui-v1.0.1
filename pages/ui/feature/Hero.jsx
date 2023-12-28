fa49d54ed8eb2fee22ae67e5f378dcf8387a876d
in-deepaknegi <negideepak.in@gmail.com>
2023-12-28 19:56:53 +0530

    updated feature component#2

--- pages/ui/feature/Hero.jsx
+++ pages/ui/feature/Hero.jsx
@@ -170,8 +170,46 @@ const Hero = () => {
 
                             <div className='col-span-2 row-start-2 min-w-0 mt-3'>
                                 {activeTab2 === 0 ? (
-                                    <section className='bg-white border rounded-2x'>
-                                        feature
+                                    <section className='overflow-hidden bg-white border rounded-2xl py-16 sm:py-20'>
+                                        <div className='mx-auto px-6 lg:px-8'>
+                                            <div className='mx-auto grid gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none grid-cols-12'>
+                                                <div className='col-span-12 lg:pr-8 lg:pt-4 lg:col-span-7'>
+                                                    <div className=''>
+                                                        <h2 className='mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl'>
+                                                            Everything you need to deploy your app
+                                                        </h2>
+                                                        <p className='mt-6 text-base text-gray-700 md:text-lg'>
+                                                            Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum pulvinar et feugiat blandit at. In mi viverra elit nunc.
+                                                        </p>
+                                                        <div className='mt-10 grid grid-cols-2 gap-x-4 max-w-xl text-base leading-7 text-gray-600 lg:max-w-none'>
+                                                            {features.slice(0, 2).map((feature) => (
+                                                                <div key={feature.name}>
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="6" height="16" x="4" y="2" rx="2"/><rect width="6" height="9" x="14" y="9" rx="2"/><path d="M22 22H2"/></svg>
+                                                                    <h2 className='my-2 text-2x font-semibold text-gray-900'>{feature.name}</h2>
+                                                                    <p>
+                                                                        {feature.description}
+                                                                    </p>
+                                                                </div>
+                                                            ))}
+                                                        </div>
+                                                        <div className="mt-10 flex gap-x-6">
+                                                            <a href="#" className="rounded-md px-3.5 py-2.5 text-sm font-semibold text-blue-600 shadow-sm ring-1 ring-blue-500 hover:text-white hover:bg-blue-500">Discover more</a>
+                                                            <a href="#" className="text-sm font-semibold leading-6 text-gray-900 my-auto">
+                                                                Join us <span aria-hidden="true">→</span></a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className='col-span-12 lg:col-span-5'>
+                                                    <Image
+                                                        src={Hero1}
+                                                        alt='Product screenshot'
+                                                        quality={100}
+                                                        loading='eager'
+                                                        className='h-[20rem] lg:h-[33rem] object-cover rounded-2xl'
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
                                     </section>
                                 ) : (
                                     <div>code</div>
