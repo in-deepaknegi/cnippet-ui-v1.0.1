13490f4a5605d31aa718dd284af9eeaeba3870f9
in-deepaknegi <negideepak.in@gmail.com>
2024-01-18 21:54:57 +0530

    updated navbar, routes init

--- app/(routes)/ui/components/navbar/page.tsx
+++ app/(routes)/ui/components/navbar/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Hero from '@/components/ui/navbar/Hero'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 const page = () => {
     return (
         <>
+            <Navbar />
             <Hero />
+            <Footer />
         </>
     )
 }
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,39 +1,59 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="bg-white mt-20">
-            <div className='py-10 mx-7'>
-                <div className='sm:flex sm:items-center sm:justify-between'>
-                    <a href="https://flowbite.com/" className='flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse'>
-                        {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" /> */}
-                        <span className='self-center text-2xl font-medium whitespace-nowrap'>CnippetUI</span>
+        <footer className="mt-20 bg-white">
+            <div className="py-10">
+                <div className="px-6 sm:flex sm:items-center sm:justify-between lg:px-8">
+                    <a
+                        href="/"
+                        className="mb-4 flex items-center space-x-3 rtl:space-x-reverse sm:mb-0">
+
+                        <span className="self-center whitespace-nowrap text-2xl font-medium">
+                            CnippetUI
+                        </span>
                     </a>
-                    <ul className='flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 '>
+                    <ul className="mb-6 flex flex-wrap items-center text-sm font-medium text-gray-700 sm:mb-0 ">
                         <li>
-                            <a href='#' className='hover:underline me-4 md:me-6'>About</a>
+                            <a href="#" className="me-4 hover:underline md:me-6">
+                                About
+                            </a>
                         </li>
                         <li>
-                            <a href='#' className='hover:underline me-4 md:me-6'>Privacy Policy</a>
+                            <a href="#" className="me-4 hover:underline md:me-6">
+                                Privacy Policy
+                            </a>
                         </li>
                         <li>
-                            <a href='#' className='hover:underline me-4 md:me-6'>Licensing</a>
+                            <a href="#" className="me-4 hover:underline md:me-6">
+                                Licensing
+                            </a>
                         </li>
                         <li>
-                            <a href='#' className='hover:underline'>Contact</a>
-                        </li>
-                        <li>
-                            <a href='/design' className='hover:underline'>Design</a>
+                            <a href="#" className="hover:underline">
+                                Contact
+                            </a>
                         </li>
                     </ul>
                 </div>
-                <hr className='my-6 border-gray-200 sm:mx-auto lg:my-8' />
-                <span className='block text-sm text-gray-500 sm:text-center dark:text-gray-400'>Designed by: <a target='_blank' href="https://www.deepaknegi.in/">Deepak Negi</a></span>
-                
-                <span className='mt-2 block text-sm text-gray-500 sm:text-center dark:text-gray-400'>© 2023 <a href='/' className='hover:underline'>CnippetUI™</a>. All Rights Reserved.</span>
+                <hr className="my-6 w-full border-gray-300 lg:my-8" />
+                <span className="block text-sm text-gray-700 sm:text-center">
+                    Designed by:{" "}
+                    <a target="_blank" href="https://www.deepaknegi.in/">
+                        Deepak Negi
+                    </a>
+                </span>
+
+                <span className="mt-2 block text-sm text-gray-700 sm:text-center">
+                    © 2024{" "}
+                    <a href="/" className="hover:underline">
+                        CnippetUi™
+                    </a>
+                    . All Rights Reserved.
+                </span>
             </div>
         </footer>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
