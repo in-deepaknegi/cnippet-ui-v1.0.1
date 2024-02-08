a6b1b1b1552fd8860a299c3d3ec608a4bf736bb8
in-deepaknegi <negideepak.in@gmail.com>
2024-02-08 23:00:14 +0530

    added pricing section-homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/(pages)/home/Hero'
 import Component from '@/components/(pages)/home/Component'
+import Pricing from '@/components/(pages)/home/Pricing'
 import Footer from '@/components/Footer'
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
       <main>
         <Hero />
         <Component />
+        <Pricing />
       </main>
       <Footer />
     </>
