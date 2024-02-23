dda84d2b61fd393b6b0822b89894f742f5e7f8fd
in-deepaknegi <negideepak.in@gmail.com>
2024-02-23 11:54:11 +0530

    updated animation in newsletter and login page

--- components/(pages)/login/Hero.jsx
+++ components/(pages)/login/Hero.jsx
@@ -52,9 +52,12 @@ const Hero = () => {
                 <div className="w-full max-w-sm">
                     <button
                         onClick={handleLogin}
-                        className="inline-flex w-full justify-center rounded-lg border px-4 py-2.5 text-base hover:bg-olive-800 hover:text-white focus:bg-black focus:text-white">
+                        className="inline-flex w-full justify-center rounded-lg border px-4 py-2.5 text-base">
                         {loading && (
-                            <span className="loader mr-2"></span>
+                            <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                            </svg>
                         )}
                         <Image
                             src={GoogleLogo}
@@ -63,10 +66,10 @@ const Hero = () => {
                             height={20}
                             className="mx-3"
                         />
-                        {status==='authenticated'? `Welcome back ${session.user.name}`: 'Log in with Google'}
+                        {status === 'authenticated' ? `Welcome back ${session.user.name}` : 'Log in with Google'}
                     </button>
                 </div>
-                <div className="my-4 flex  max-w-sm items-center">
+                <div className="my-4 flex max-w-sm items-center">
                     <div className="h-[0.025rem] w-[24rem] bg-gray-200"></div>
                     <div className="mx-2">or</div>
                     <div className="h-[0.025rem] w-full bg-gray-200"></div>
@@ -74,7 +77,7 @@ const Hero = () => {
 
                 <form action="/login" className="form w-full max-w-sm">
                     <p className="text-sm text-center">Email Auth is disabled as of this moment.</p>
-                    <div className="mb-6">
+                    <div className="mb-6 space-y-2">
                         <label htmlFor="email">Email address</label>
                         <input
                             type="email"
@@ -84,7 +87,7 @@ const Hero = () => {
                             required=""
                             disabled />
                     </div>
-                    <div className="mb-6 ">
+                    <div className="mb-6 space-y-2">
                         <label htmlFor="password">Password</label>
                         <input
                             type="password"
--- components/Newsletter.jsx
+++ components/Newsletter.jsx
@@ -26,7 +26,11 @@ const Newsletter = () => {
         setSuccess(success);
         setMessage(msg);
         setLoading(false);
-        setEmail("");
+
+        setTimeout(() => {
+            setEmail("");
+            setMessage("");
+        }, 4000);
     }
 
     return (
@@ -65,8 +69,15 @@ const Newsletter = () => {
                         />
                         <button
                             type="submit"
-                            className="flex-none rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500"
+                            className="flex rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500"
                         >
+                            {loading && (
+                                <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                </svg>
+                            )}
+
                             Subscribe
                         </button>
                     </div>
